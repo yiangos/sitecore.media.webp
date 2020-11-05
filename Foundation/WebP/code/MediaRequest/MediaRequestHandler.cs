8a2a7552e8cea3f28d84e5214ccae3124e77ba5e
Ioannis Vavouranakis <y.vavouranakis@linakis.com>
2020-11-05 23:41:19 +0200

    initial commit

---
+++ Foundation/WebP/code/MediaRequest/MediaRequestHandler.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Yiangos.Foundation.MediaWebP.MediaRequest
+{
+    // Sitecore.Resources.Media.MediaRequestHandler
+    using Sitecore;
+    using Sitecore.Configuration;
+    using Sitecore.Data;
+    using Sitecore.Data.Items;
+    using Sitecore.Diagnostics;
+    using Sitecore.Events;
+    using Sitecore.Globalization;
+    using Sitecore.Pipelines.GetResponseCacheHeaders;
+    using Sitecore.Resources;
+    using Sitecore.Resources.Media;
+    using Sitecore.Resources.Media.Streaming;
+    using Sitecore.Security.Accounts;
+    using Sitecore.SecurityModel;
+    using Sitecore.Sites;
+    using Sitecore.Text;
+    using Sitecore.Web;
+    using Sitecore.Web.Authentication;
+    using System;
+    using System.Collections.Generic;
+    using System.Web;
+
+    /// <summary>
+    /// MediaRequestHandler class
+    /// </summary>
+    public class MediaRequestHandler : Sitecore.Resources.Media.MediaRequestHandler
+    {
+        /// <summary>
+        /// Executes the process request event.
+        /// </summary>
+        /// <param name="context">
+        /// The context.
+        /// </param>
+        /// <param name="request">
+        /// The request.
+        /// </param>
+        /// <param name="media">
+        /// The Sitecore media.
+        /// </param>
+        /// <returns>
+        /// The <see cref="T:System.Boolean" /> that indicates whether request has been processed.
+        /// </returns>
+        protected override bool DoProcessRequest(HttpContext context, MediaRequest request, Media media)
+        {
+            Assert.ArgumentNotNull(context, "context");
+            Assert.ArgumentNotNull(request, "request");
+            Assert.ArgumentNotNull(media, "media");
+            SetWebpOptions(request, context);
+            return base.DoProcessRequest(context, request, media);
+        }
+
+        private bool AcceptWebP(HttpContext context)
+        {
+            Sitecore.Diagnostics.Log.Info("::MEDIA:: webp enabled:" + (context?.Request.AcceptTypes != null && (context.Request.AcceptTypes).Contains("image/webp")).ToString() + ",  user Agent:" + context.Request.UserAgent + ", raw accept-types:" + string.Join(", ", context.Request.AcceptTypes), this);
+            return context?.Request.AcceptTypes != null && (context.Request.AcceptTypes).Contains("image/webp");
+        }
+
+        /// <summary>
+        /// Processes the accept webp option for dynamically scaled images
+        /// </summary>
+        /// <param name="context">The HttpContext.</param>
+        private void SetWebpOptions(MediaRequest request, HttpContext context)
+        {
+            Assert.ArgumentNotNull(request, "request");
+            if (AcceptWebP(context))
+            {
+                request.Options.CustomOptions["webp"] = "1";
+            }
+        }
+    }
+}
---
+++ Foundation/WebP/code/Pipelines/getMediaStream/ConvertToWebP.cs
@@ -0,0 +1,59 @@
+﻿using ImageProcessor;
+using ImageProcessor.Imaging.Formats;
+using ImageProcessor.Plugins.WebP.Imaging.Formats;
+using Sitecore.Diagnostics;
+using Sitecore.Resources.Media;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace Yiangos.Foundation.MediaWebP.Pipelines.getMediaStream
+{
+    public class ConvertToWebPProcessor
+    {
+        public void Process(GetMediaStreamPipelineArgs args)
+        {
+            Sitecore.Diagnostics.Log.Debug("::WEBP:: enabled:" + (args.Options.CustomOptions.ContainsKey("webp") || args.Options.CustomOptions["webp"] == "1").ToString(), this);
+            Assert.ArgumentNotNull(args, "args");
+            if (!ShouldSkip(args))
+            {
+                Sitecore.Diagnostics.Log.Debug("::WEBP:: converting " +args.MediaData.MediaItem.InnerItem.Paths.Path, this);
+                ISupportedImageFormat format = new WebPFormat() { Quality = Sitecore.Configuration.Settings.GetIntSetting("Media.Resizing.Quality", 70) };
+                MemoryStream outstream = new MemoryStream();
+                using (Stream stream = args.MediaData.MediaItem.GetMediaStream())
+                {
+                    using (ImageFactory imageFactory = new ImageFactory(preserveExifData: true))
+                    {
+                        imageFactory.Load(stream).Format(format).Save(outstream);
+                        args.OutputStream = new MediaStream(outstream, "webp", args.MediaData.MediaItem);
+                        args.OutputStream.Headers.Headers["Content-Type"] = "image/webp";
+                    }
+                }
+
+            }
+        }
+        protected bool ShouldSkip(GetMediaStreamPipelineArgs args)
+        {
+            MediaData mediaData = args.MediaData;
+            if (!mediaData.MimeType.StartsWith("image/", StringComparison.Ordinal))
+            {
+                return true;
+            }
+            if (mediaData.MimeType.StartsWith("image/webp", StringComparison.Ordinal))
+            {
+                return true;
+            }
+            if (!args.Options.CustomOptions.ContainsKey("webp") || args.Options.CustomOptions["webp"] != "1")
+            {
+                return true;
+            }
+            if (!args.MediaData.HasContent)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
